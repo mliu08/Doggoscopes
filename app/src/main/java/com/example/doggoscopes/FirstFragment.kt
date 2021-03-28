48413828fea8883d1a1ce4126c14185a58596b9c
coriegulik <coriegulik@gmail.com>
2021-03-27 23:35:45 -0700

    something

--- app/src/main/java/com/example/doggoscopes/FirstFragment.kt
+++ app/src/main/java/com/example/doggoscopes/FirstFragment.kt
@@ -13,8 +13,10 @@ import androidx.navigation.fragment.findNavController
  */
 class FirstFragment : Fragment() {
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-            savedInstanceState: Bundle?): View? {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.title_screen, container, false)
 
@@ -29,6 +31,12 @@ class FirstFragment : Fragment() {
 
         view.findViewById<Button>(R.id.ariesButton).setOnClickListener {
             findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
+        }
+        view.findViewById<Button>(R.id.taurusButton).setOnClickListener {
+            findNavController().navigate(R.id.action_FirstFragment_to_TaurusFragment)
+        }
+        view.findViewById<Button>(R.id.geminiButton).setOnClickListener {
+            findNavController().navigate(R.id.action_FirstFragment_to_GeminiFragment)
         }
     }
 }
